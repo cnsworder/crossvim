20bfdd89859d8cea7a373a2e4d3d207cdb4ff331
crossorbit <cnsworder@gamil.com>
2016-01-21 15:54:07 +0800

    Add setup vim plugin

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if -e ~/.vimrc;then
+    rm ~/.vimrc
+fi
+ln -s vimrc ~/.vimrc
+
+if ! -e ~/.vim/bundle; then
+    mkdir -p ~/.vim/bundle
+fi    
+if ! -e ~/.vim/bundle/vundle; then    
+    ln -s vundle ~/.vim/bundle/vundle 
+fi
+
+vim +PluginInstall +qall
+
+
+
